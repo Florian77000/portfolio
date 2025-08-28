93932f8112e8ffc52033d48e0cc2dc8ae4278b73
unknown <floriandelaunay75@gmail.com>
2025-08-28 15:33:48 +0200

    css home page

--- components/Home.js
+++ components/Home.js
@@ -5,18 +5,20 @@ function Home() {
     <div className={styles.main}>
 
       {/* Barre de navigation du site */}
-      <navbar className={styles.navbar}>
+      <div className={styles.navbar}>
         <li>A propos</li>
         <li>Mes formations</li>
         <li>Mes projets</li>
         <li>Contact</li>
-      </navbar>
+      </div>
       
       {/* Partie de présentation du site, accroche du site */}
       <div className={styles.hero}>
-        <h1>Bonjour je m'appelle Florian,
-           concepteur développeur web junior.</h1>
-           <h3>
+        <div className={styles.h1}>
+          <h1 className={styles.textH1}>Bonjour je m'appelle Florian, concepteur développeur web junior.</h1>
+        </div>
+        <div className={styles.h3}>
+          <h3 className={styles.textH3}>
              Passionné par la création de solutions modernes et 
              performantes. je vous accompagne dans toutes les étapes 
              de votre projet digital : de l’analyse du besoin à la 
@@ -25,15 +27,16 @@ function Home() {
              à la fois esthétiques, intuitifs et robustes, tout en 
              garantissant une expérience utilisateur optimale.
            </h3>
+        </div>  
       </div>
 
       <div className={styles.button}>
-        <button>Contactez moi</button>
+        <button className={styles.textButton}>Contactez moi</button>
       </div>
 
       <div className={styles.image}>
-        <img src='' alt='lien linkedin'></img>
-        <img src='' alt='lien github'></img>
+        <img className={styles.logo} src='./github.png' alt='lien linkedin'></img>
+        <img className={styles.logo} src='./linkedin.png' alt='lien github'></img>
       </div>
     </div>
   );
