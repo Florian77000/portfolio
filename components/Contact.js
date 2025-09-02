ff61bdcb3a90dcdb1a354e4daa7a8ddcb867ca22
unknown <floriandelaunay75@gmail.com>
2025-09-02 18:35:26 +0200

    add useState for input, test ok onclick

--- components/Contact.js
+++ components/Contact.js
@@ -1,17 +1,33 @@
-import styles from '../styles/Contact.module.css'
+import styles from '../styles/Contact.module.css';
+import { useState } from 'react';
 
 export default function Contact () {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [select, setSelect] = useState('');
+
+    const handleClick = () => {
+        setName('');
+        setEmail('');
+        setSelect('');
+        console.log(name, email, select)
+    }
+
     return (
         <div className={styles.mainContact}>
             <h1 className={styles.h1}>Contact</h1>
             <div className={styles.containerContact}>
                 <div className={styles.form}>
-                    <input className={styles.input} placeholder='nom'></input>
-                    <input className={styles.input} placeholder='email'></input>
-                    <input className={styles.input} placeholder='recherche'></input>
+                    <input className={styles.input} placeholder='nom' onChange={(e) => setName(e.target.value)} value={name}></input>
+                    <input className={styles.input} placeholder='email' onChange={(e)=> setEmail(e.target.value)} value={email}></input>
+                    <select className={styles.input} onChange={(e) => setSelect(e.target.value)} value={select}>
+                        <option></option>
+                        <option value='mise en relation'>Mise en relation</option>
+                        <option value='project application'>Porjet application</option>
+                    </select>
                 </div>
                 <div className={styles.formBtn}>
-                    <button className={styles.btn}>Envoyer</button>
+                    <button onClick={() =>handleClick()} className={styles.btn}>Envoyer</button>
                 </div>
             </div>
         </div>
