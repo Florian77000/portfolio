b275ecaf00ff304a4ed15af5ade825a34adf24f5
unknown <floriandelaunay75@gmail.com>
2025-09-02 17:43:46 +0200

    add card css

--- components/Project.js
+++ components/Project.js
@@ -3,24 +3,29 @@ import styles from '../styles/Project.module.css'
 export default function Project () {
     return (
         <div className={styles.mainProject}>
-            <h1>Mes projets</h1>
-            <div className={styles.card}>
-                <div className={styles.cardImage}>
-                    affiche image
-                </div>
-                <div className={styles.cardTexte}>
-                    <div className={styles.title}>
-                        <h3>Titre</h3>
-                        <p>Texte d'explication</p>
+            <div className={styles.mainTitle}>
+                 <h1>Mes projets</h1>
+            </div>
+            <div className={styles.cardContainer}>
+                <div className={styles.card}>
+                    <div className={styles.cardImage}>
+                        <img className={styles.img} src='./computer.jpg'></img>
                     </div>
-                    <div className={styles.cardBtn}>
-                        <button className={styles.btn}>bouton 1</button>
-                        <button className={styles.btn}>bouton 2</button>
-                        <button className={styles.btn}>bouton 3</button>
-                    </div>    
+                    <div className={styles.cardTexte}>
+                        <div className={styles.title}>
+                            <h3 className={styles.h3}>Titre</h3>
+                            <p className={styles.description}>Texte d'explication</p>
+                        </div>
+                        <div className={styles.cardBtn}>
+                            <button className={styles.btn}>bouton 1</button>
+                            <button className={styles.btn}>bouton 2</button>
+                            <button className={styles.btn}>bouton 3</button>
+                        </div>    
+                    </div>
                 </div>
 
             </div>
+            
         </div>
     )
 }
