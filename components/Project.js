5ecc8b68543b61cdff01bfd3199b11148dc53c8e
unknown <floriandelaunay75@gmail.com>
2025-09-03 18:51:49 +0200

    add ux design on card, effect zoom with cursor

--- components/Project.js
+++ components/Project.js
@@ -13,34 +13,32 @@ export default function Project () {
     }, []);
 
     const viewProject = project.map((e,i) => (
-        <div className={styles.mainProject} key={i}>
-            <div className={styles.mainTitle}>
-                 <h1>Mes projets</h1>
-            </div>
-            <div className={styles.cardContainer}>
-                <div className={styles.card}>
-                    <div className={styles.cardImage}>
-                        <img className={styles.img} src='./computer.jpg'></img>
+        <div className={styles.cardContainer} key={i}>
+            <div className={styles.card}>
+                <div className={styles.cardImage}>
+                    <img className={styles.img} src='./computer.jpg'></img>
+                </div>
+                <div className={styles.cardTexte}>
+                    <div className={styles.title}>
+                        <h3 className={styles.h3}>{e.title}</h3>
+                        <p className={styles.description}>{e.description}</p>
                     </div>
-                    <div className={styles.cardTexte}>
-                        <div className={styles.title}>
-                            <h3 className={styles.h3}>{e.title}</h3>
-                            <p className={styles.description}>{e.description}</p>
-                        </div>
-                        <div className={styles.cardBtn}>
-                          {e.button.map((btn, idx) => (
-                            <button key={idx} className={styles.btn}>
-                            {btn}
-                            </button>
-                          ))}
-                        </div>    
-                    </div>
+                    <div className={styles.cardBtn}>
+                        {e.button.map((btn, idx) => (
+                        <button key={idx} className={styles.btn}>
+                        {btn}
+                        </button>
+                        ))}
+                    </div>    
                 </div>
             </div>
         </div>
-        
-    ))
+    ));
     return (
-        <>{viewProject}</>
-    )
-}
+        <div className={styles.mainProject}>
+            <div className={styles.mainTitle}>
+                 <h1>Mes projets</h1>
+            </div>
+        {viewProject}
+    </div>
+    )};
