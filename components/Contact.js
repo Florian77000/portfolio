cadc5aaca5580b4ef44a1b97c16fc5b3473e5cb9
unknown <floriandelaunay75@gmail.com>
2025-09-06 18:27:07 +0200

    add message with textarea, test ok

--- components/Contact.js
+++ components/Contact.js
@@ -6,14 +6,15 @@ export default function Contact () {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [select, setSelect] = useState('');
+    const [message, setMessage] = useState('')
 
-    const handleClick = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         // preventDefault dit au navigateur : " ne fais pas ton comportement par défaut (reload), laisse-moi gérer l’action moi-même en JavaScript".
         emailjs.send(
             process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
             process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
-            { name, email, select },
+            { name, email, select, message },
             process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
         )
         .then(() => {
@@ -22,6 +23,8 @@ export default function Contact () {
             setName('');
             setEmail('');
             setSelect('');
+            setMessage('');
+            console.log('message', message)
         }) 
     }
 
@@ -29,7 +32,7 @@ export default function Contact () {
         <div id="contact" className={styles.mainContact}>
             <h1 className={styles.h1}>Contact</h1>
             <div className={styles.containerContact}>
-                <div className={styles.form}>
+                <form onSubmit={handleSubmit} className={styles.form}>
                     <input className={styles.input} placeholder='nom' onChange={(e) => setName(e.target.value)} value={name}></input>
                     <input className={styles.input} placeholder='email' onChange={(e)=> setEmail(e.target.value)} value={email}></input>
                     <select className={styles.input} onChange={(e) => setSelect(e.target.value)} value={select}>
@@ -37,10 +40,11 @@ export default function Contact () {
                         <option value='mise en relation'>Mise en relation</option>
                         <option value='project application'>Projet application</option>
                     </select>
-                </div>
-                <div className={styles.formBtn}>
-                    <button onClick={(e) =>handleClick(e)} className={styles.btn}>Envoyer</button>
-                </div>
+                    <textarea className={styles.input} placeholder='Precisez votre demande' onChange={(e) => setMessage(e.target.value)} value={message}></textarea>
+                    <div className={styles.formBtn}>
+                        <button type='submit' className={styles.btn}>Envoyer</button>
+                    </div>
+                </form>    
             </div>
         </div>
     )
